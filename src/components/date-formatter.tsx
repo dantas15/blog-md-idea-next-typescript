9e7b8b6dfc76d1e778f69a1fb56a8ce69883a9f9
Gustavo Galote <gusgalot3g@gmail.com>
2022-11-11 10:14:41 -0300

    chore(styles): minor adjustments

--- src/components/date-formatter.tsx
+++ src/components/date-formatter.tsx
@@ -6,5 +6,5 @@ type Props = {
 
 export function DateFormatter({ dateString }: Props) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return <time className="text-zinc-700" dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,11 +7,11 @@ type Props = {
 
 export function Layout({ children }: Props) {
   return (
-    <div className='relative min-h-screen'>
+    <div className="relative min-h-screen">
       {/* TODO: Meta tags */}
       <Header />
-      <div className="w-full items-center pb-16">
-        <main className='max-w-xl mx-auto px-4 md:px-0'>{children}</main>
+      <div className="w-full items-center pb-64">
+        <main className="max-w-xl mx-auto px-4 md:px-0">{children}</main>
       </div>
       <Footer />
     </div>
